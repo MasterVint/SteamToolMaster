e7223b6a1d5ad4ace50f86022e96fdf5dc836648
MasterVint <174651822+MasterVint@users.noreply.github.com>
2025-02-24 23:16:14 +0100

    Debugged DataClass.h::ToolSettings
    Cleaned up redundant code in the AddAlias function
    Fixed Attempting to free already freed memory in ToolsSettings deconstructor

--- SteamToolsMaster/SteamToolsMaster/DataClass.h
+++ SteamToolsMaster/SteamToolsMaster/DataClass.h
@@ -2,32 +2,30 @@
 #define DATACLASS_H
 #include <iostream>
 //a class for the data such as settings, keyprice and command aliases
+// 
 class ToolSettings {
 private:
     // Dynamic arrays to store the aliases
-    const char** TradePriceAlias;
+	const char** TradePriceAlias;
     const char** KitMakerAlias;
     const char** SetKeyAlias;
 
     // Track the size of each array
     size_t TradePriceCount;
-    size_t KitMakerCount;
+    size_t KitMakerCount;	
     size_t SetKeyCount;
 
-    size_t current_cap = 1;
-
-    size_t metal_keyprice[4] = {60,2,2,1}; // Refined, Reclaimed, Scrap, Weapons
-    size_t USD_keyprice = 178; // divide by 100 for actual price
+    size_t INITIAL_CAP = 1;
 public:
     ToolSettings() {
-        TradePriceAlias = new const char* [current_cap];
-        KitMakerAlias = new const char* [current_cap];
-        SetKeyAlias = new const char* [current_cap];
-        TradePriceCount = 1;
-        KitMakerCount = 1;
-        SetKeyCount = 1;
+        TradePriceAlias = new const char* [INITIAL_CAP];
+        KitMakerAlias = new const char* [INITIAL_CAP];
+        SetKeyAlias = new const char* [INITIAL_CAP];
+        TradePriceCount = 0;
+        KitMakerCount = 0;
+        SetKeyCount = 0;
 
-        for (size_t i = 0; i < current_cap; i++) {
+        for (size_t i = 0; i < INITIAL_CAP; i++) {
             TradePriceAlias[i] = nullptr;
             KitMakerAlias[i] = nullptr;
             SetKeyAlias[i] = nullptr;
@@ -35,80 +33,34 @@ public:
     }
 
     ~ToolSettings() {
-        // Clean up dynamically allocated memory
-        for (size_t i = 0; i < TradePriceCount; i++) {
-            delete[] TradePriceAlias[i];
-        }
+
         delete[] TradePriceAlias;
-
-        for (size_t i = 0; i < KitMakerCount; i++) {
-            delete[] KitMakerAlias[i];
-        }
         delete[] KitMakerAlias;
-
-        for (size_t i = 0; i < SetKeyCount; i++) {
-            delete[] SetKeyAlias[i];
-        }
         delete[] SetKeyAlias;
     }
 
-    const char** resizeArray(const char** arr, size_t& count, size_t& new_capacity) {
-        const char** temp = new const char* [new_capacity];
-        for (size_t i = 0; i < count; i++) {
-            temp[i] = arr[i];
+    const char** resizeArray(const char** old_array, size_t &oldsize, size_t &newsize) {
+        const char** new_array = new const char* [newsize];
+        for (size_t i = 0; i < oldsize; i++) {
+            new_array[i] = old_array[i];
         }
-        delete[] arr;
-        return temp;
+        delete[] old_array;
+        return new_array;
     }
 
-    void AddAlias(const char* value, const char* category) {
-        size_t newsize = 0;
-        const char** targetArray;
-        size_t count = 0;
-
+    void AddAlias(const char* new_value, const char* category) {
+        size_t InputCount = 0;
         if (strcmp(category, "TradePriceAlias") == 0) {
-            //sgiam
-            std::cout << "!iim here" << "\n";
-            targetArray = TradePriceAlias;
-            count = TradePriceCount;
-            TradePriceCount += 1;
-            newsize = TradePriceCount;
-            if (newsize > current_cap) {
-                TradePriceAlias = resizeArray(TradePriceAlias, count, newsize);
+            InputCount = TradePriceCount + 1;
+            //we want to add a single item to TradePriceAlias
+            //resize to new size
+            if (InputCount > INITIAL_CAP) {
+                TradePriceAlias = resizeArray(TradePriceAlias, TradePriceCount, InputCount);
+                //this means that our array is too small for our new_value, thus we resize
             }
+            TradePriceAlias[TradePriceCount] = new_value;
+            TradePriceCount++;
         }
-        else if (strcmp(category, "KitMakerAlias") == 0) {
-            targetArray = KitMakerAlias;
-            count = KitMakerCount;
-            KitMakerCount += 1;
-            newsize = KitMakerCount;
-            if (newsize > current_cap) {
-                KitMakerAlias = resizeArray(KitMakerAlias, count, newsize);
-            }
-        }
-        else if (strcmp(category, "SetKeyAlias") == 0) {
-            targetArray = SetKeyAlias;
-            count = SetKeyCount;
-            SetKeyCount += 1;
-            newsize = SetKeyCount;
-            if (newsize > current_cap) {
-                SetKeyAlias = resizeArray(SetKeyAlias, count, newsize);
-            }
-        }
-        else {
-            return;
-        }
-
-        size_t valueLen = strlen(value) + 1;
-        targetArray[count] = new char[valueLen];
-       // strcpy();
-       // strcpy((char*)targetArray[count],,value);
-        strcpy_s((char*)targetArray[count], sizeof(targetArray[count]), value);
-        count++;
-        for (size_t i = 0; i < count; i++) {
-            std::cout << targetArray[i] << '\n';
-        }
-
     }
 
     const char** GetTradePriceAlias() { return TradePriceAlias; };
--- SteamToolsMaster/SteamToolsMaster/Source.cpp
+++ SteamToolsMaster/SteamToolsMaster/Source.cpp
@@ -8,16 +8,19 @@ int main(int argc, const char* argv[]) {
 	argv[1] = "test";
 	argv[2] = "hello";
 	argc = 3;
-
-	ToolSettings Settings;
-
-	Settings.AddAlias("sigma", "TradePriceAlias");
+	//Check for memory leak with this
+	for (size_t i = 0; i < 1000000000; i++) {
+		ToolSettings Settings;
+		Settings.AddAlias("sigma", "TradePriceAlias");
+		std::cout << "sigma" << std::endl;
+	}
+	std::cout << "end" << "\n";
 	if (argc <= 1) {
 		printf("usage: stmtool [ tradeprice | trp ] [ kitmaker | ktm ]\n\n");
 		printf("See \"stmtool [command] help\" for specific help");
 		return 0;
 	}
-
+	std::cout << "end" << "\n";
 	//Parse Settings.cfg
 	if (strcmp(argv[1], "tradeprice") == 0|| strcmp(argv[1], "trp") == 0) {
 		if (argc <= 3 && strcmp(argv[2], "help")) {
@@ -33,6 +36,7 @@ int main(int argc, const char* argv[]) {
 		}
 
 	}
+	std::cout << "end" << "\n";
 	return 0;
 
 }
